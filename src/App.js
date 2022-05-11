9d55427754c38cbb76bb479570316f54c5efe77b
msuero1906 <43377325+msuero1906@users.noreply.github.com>
2022-05-11 18:25:33 -0400

    Finished making buttons and started on functionality

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
 import "./styles.css";
+import { useState } from "react";
 
 const ResultScreen = () => {
   return <div>Result!</div>;
 };
 
-const Button = () => {
-  return <button>Click me!</button>;
+const Button = (props) => {
+  return (
+    <div>
+      {props.buttonsArray.map((elem) => {
+        return (
+          <button onClick={() => console.log("Button Clicked")}>{elem}</button>
+        );
+      })}
+    </div>
+  );
 };
 
 export default function App() {
-  const buttonsArr = [
-    [1, 2, 3, "+"],
-    [4, 5, 6, "-"],
-    [7, 8, 9, "*"],
-    [0, "/"],
-    ["Clear", "="]
+  // State changes: numbers, operations, result
+  const [numbers, setNumbers] = useState(0);
+  const [operations, setOperations] = useState("");
+  const [result, setResult] = useState(0);
+  const buttonsArray = [
+    1,
+    2,
+    3,
+    "+",
+    4,
+    5,
+    6,
+    "-",
+    7,
+    8,
+    9,
+    "*",
+    0,
+    "/",
+    "Clear",
+    "="
   ];
   return (
     <div className="App">
@@ -22,7 +46,7 @@ export default function App() {
       <div className="wrapper">
         <h1>Calculator Container</h1>
         <ResultScreen />
-        <Button />
+        <Button buttonsArray={buttonsArray} />
       </div>
     </div>
   );
